b7734f93d8503118e7a620bc64811a08eb5db644
Champ <kuldeep3727@gmail.com>
2023-09-14 05:58:52 +0530

    Feature commit

--- src/main/java/org/example/DevTestClass.java
+++ src/main/java/org/example/DevTestClass.java
@@ -4,5 +4,6 @@ public class DevTestClass {
     public static void main(String[] args) {
         System.out.println("DevTestClass");
         System.out.println("DevTestClass2");
+        System.out.println("Added feature changes");
     }
 }
